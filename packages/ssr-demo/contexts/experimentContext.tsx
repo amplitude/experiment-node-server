a4a5610ac426be9a30e2aafd4739c1d86432bac6
Brian Giori <brian.giori@amplitude.com>
2021-06-01 18:12:31 -0700

    fix warning

--- packages/ssr-demo/contexts/experimentContext.tsx
+++ packages/ssr-demo/contexts/experimentContext.tsx
@@ -2,6 +2,7 @@ import { createContext } from 'react';
 
 export const ExperimentContext = createContext(null);
 
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 export const ExperimentProvider = (props) => {
   const { value, children } = props;
   return (
