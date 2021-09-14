4764c1e77ade9472833f63da2ed3a668474d1c2d
Brian Giori <brian.giori@amplitude.com>
2021-09-13 18:10:38 -0700

    use beforeAll

--- packages/node/test/localEvaluation.test.ts
+++ packages/node/test/localEvaluation.test.ts
@@ -6,7 +6,10 @@ const apiKey = 'server-qz35UwzJ5akieoAdIgzM4m9MIiOLXLoz';
 const testUser: ExperimentUser = { user_id: 'test_user' };
 
 const client = Experiment.initializeLocal(apiKey);
-client.stopFlagConfigPoller();
+
+beforeAll(() => {
+  client.stopFlagConfigPoller();
+});
 
 test('ExperimentClient.evaluate, success', async () => {
   const variants = await client.evaluate(testUser);
