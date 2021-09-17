3c5d8342d5e613fb1db3a5af5629aa03e9604132
Brian Giori <brian.giori@amplitude.com>
2021-09-17 11:20:41 -0700

    export local evaluation stuff

--- packages/node/src/index.ts
+++ packages/node/src/index.ts
@@ -11,3 +11,8 @@ export { ExperimentConfig, Defaults } from './config';
 export { Experiment } from './factory';
 export { ExperimentUser } from './types/user';
 export { Variant, Variants } from './types/variant';
+
+export { LocalEvaluationClient } from './localEvaluation';
+export { FlagConfigCache } from './types/cache';
+export { LocalEvaluationConfig, LocalEvaluationDefaults } from './config';
+export { FlagConfig } from './types/cache';
