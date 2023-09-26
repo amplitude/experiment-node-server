5a75cafd842dd6a5866c870b6447fb52bdcedc67
Tim Yiu <tim.yiu@Tims-MacBook-Pro.local>
2023-09-26 16:50:20 -0700

    Update AmplitudeCookie util to support new cookie format
    update tests
    
    update parse() to account for JSON error
    
    style changes

--- packages/node/test/cookie.test.ts
+++ packages/node/test/cookie.test.ts
@@ -1,10 +1,17 @@
 import { AmplitudeCookie } from 'src/cookie';
 
+const userAndDeviceNewCookie =
+  'JTdCJTIydXNlcklkJTIyJTNBJTIydGVzdCU0MGFtcGxpdHVkZS5jb20lMjIlMkMlMjJkZXZpY2VJZCUyMiUzQSUyMmRldmljZUlkJTIyJTdE';
+const deviceNewCookie = 'JTdCJTIyZGV2aWNlSWQlMjIlM0ElMjJkZXZpY2VJZCUyMiU3RA==';
+
 test('AmplitudeCookie.cookieName', () => {
   expect(() => {
     AmplitudeCookie.cookieName('');
   }).toThrowError();
   expect(AmplitudeCookie.cookieName('1234567')).toEqual('amp_123456');
+  expect(AmplitudeCookie.cookieName('1234567890', true)).toEqual(
+    'AMP_1234567890',
+  );
 });
 
 test('AmplitudeCookie.parse', () => {
@@ -13,7 +20,10 @@ test('AmplitudeCookie.parse', () => {
   ).toEqual({
     device_id: 'deviceId',
   });
-
+  expect(AmplitudeCookie.parse(deviceNewCookie, true)).toEqual({
+    device_id: 'deviceId',
+  });
+  expect(AmplitudeCookie.parse('invalidcookie', true)).toEqual({});
   expect(
     AmplitudeCookie.parse(
       'deviceId.dGVzdEBhbXBsaXR1ZGUuY29t..1f1gkeib1.1f1gkeib1.dv.1ir.20q',
@@ -22,7 +32,10 @@ test('AmplitudeCookie.parse', () => {
     device_id: 'deviceId',
     user_id: 'test@amplitude.com',
   });
-
+  expect(AmplitudeCookie.parse(userAndDeviceNewCookie, true)).toEqual({
+    device_id: 'deviceId',
+    user_id: 'test@amplitude.com',
+  });
   expect(
     AmplitudeCookie.parse('deviceId.Y8O3Pg==..1f1gkeib1.1f1gkeib1.dv.1ir.20q'),
   ).toEqual({
@@ -33,4 +46,5 @@ test('AmplitudeCookie.parse', () => {
 
 test('AmplitudeCookie.generate', () => {
   expect(AmplitudeCookie.generate('deviceId')).toEqual('deviceId..........');
+  expect(AmplitudeCookie.generate('deviceId', true)).toEqual(deviceNewCookie);
 });
