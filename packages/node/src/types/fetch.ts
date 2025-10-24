9eb9c27090256166f8092ab2d6b43bb7efbaaddc
Peter Zhu <peter.zhu@amplitude.com>
2025-10-24 02:05:41 -0700

    feat: add tracks assignment and exposure

--- packages/node/src/types/fetch.ts
+++ packages/node/src/types/fetch.ts
@@ -6,4 +6,14 @@ export type FetchOptions = {
    * Specific flag keys to evaluate and set variants for.
    */
   flagKeys?: string[];
+
+  /**
+   * Whether to track exposure events for the request.
+   */
+  tracksExposure?: boolean;
+
+  /**
+   * Whether to track assignment events for the request.
+   */
+  tracksAssignment?: boolean;
 };
