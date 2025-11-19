ad7199491f79db327df3bdda2ecb9a770931e050
Peter Zhu <peter.zhu@amplitude.com>
2025-11-19 15:39:00 -0800

    docs: add comments

--- packages/node/src/types/fetch.ts
+++ packages/node/src/types/fetch.ts
@@ -9,11 +9,13 @@ export type FetchOptions = {
 
   /**
    * Whether to track exposure events for the request.
+   * If not provided, the default is not to track exposure events.
    */
   tracksExposure?: boolean;
 
   /**
    * Whether to track assignment events for the request.
+   * If not provided, the default is to track assignment events.
    */
   tracksAssignment?: boolean;
 };
